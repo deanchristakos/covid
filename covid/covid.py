3e9946732da98e100c76ae89b3cedfb346018b10
dean <dean@alum.mit.edu>
2020-03-25 16:01:21 -0400

    updated to get data from covidtracking.com

--- covid/covid.py
+++ covid/covid.py
@@ -3,13 +3,44 @@ from astor_globals import *
 from astor_square_utils import *
 from marshmallow import Schema, fields
 import json
+import urllib.request
+import datetime
+
+
+def get_covid_tracking_data():
+    url = 'https://covidtracking.com/api/states'
+    result = {}
+    with urllib.request.urlopen(url) as state_data:
+        data = json.loads(state_data.read().decode())
+        for entry in data:
+            state = entry['state']
+            positive = entry['positive']
+            case_date = datetime.datetime.strptime(entry['dateModified'], "%Y-%m-%dT%H:%M:%SZ").date()
+            hospitalized = entry['hospitalized']
+            death = entry['death']
+            result[state] = {'positive': positive, 'date': case_date, 'hospitalized': hospitalized, 'deaths':death}
+    return result
+
+
+def update_covid_from_entry(covid, entry):
+    covid.official_cases = entry['positive']
+    covid.official_cases_date = entry['date']
+    covid.official_deaths = entry['deaths']
+    covid.official_deaths_date = entry['date']
+    covid.hospitalized = entry['hospitalized']
+    covid.hospitalized_date = entry['date']
+    return
+
 
 class CovidStates(object):
 
     def __init__(self, dbconnection=None):
         self.covid_states = []
-
+        self.updated_date = None
         self.query = "SELECT * from state_info"
+
+        covid_tracking_data = get_covid_tracking_data()
+
         if dbconnection is not None:
             self.dbconnection = dbconnection
             cursor = dbconnection.cursor()
@@ -18,6 +49,18 @@ class CovidStates(object):
             for row in rows:
                 covid = Covid()
                 self.create_covid_from_row(covid, row)
+
+                try:
+                    covid_tracking_entry = covid_tracking_data[covid.state_abbrev]
+                    update_covid_from_entry(covid, covid_tracking_entry)
+                    if (self.updated_date is None or covid_tracking_entry.official_cases_date > self.updated_date):
+                        self.updated_date = covid.official_cases_date.strftime("%Y-%m-%d")
+                except KeyError as e:
+                    logging.error('cannot find covid tracking data for ' + covid.state_abbrev)
+                except Exception as e:
+                    logging.error('miscellanesous error: ' + str(e))
+
+
                 self.covid_states.append(covid)
         return
 
@@ -37,7 +80,7 @@ class CovidStates(object):
         return
 
     def get_json(self):
-        result = [c.get_json() for c in self.covid_states]
+        result = {'date':self.updated_date, 'states':[c.get_json() for c in self.covid_states]}
         return result
 
 class Covid(object):
@@ -76,6 +119,9 @@ class Covid(object):
         self.days_till_hospitals_full = None
         self.beds_per_1000 = None
         self.beds_per_1000_formatted = None
+
+        self.hospitalized = None
+        self.hospitalized_date = None
         return
 
     def get_json(self):
